f634100b4ff06c20f127524fb861a0f56cc6a7ed
JeroenBongers <jeroen.c.bongers@gmail.com>
2023-08-09 22:41:44 +0200

    feat: Updated project layout to a more commonly used structure and created the first DI extension method

---
+++ src/AzureEz.ServiceBus/ServiceCollectionExtensions.cs
@@ -0,0 +1,39 @@
+﻿using AzureEz.ServiceBus.Infrastructure;
+using AzureEz.ServiceBus.Infrastructure.Interfaces;
+using Microsoft.Extensions.Azure;
+using Microsoft.Extensions.Configuration;
+using Microsoft.Extensions.DependencyInjection;
+
+namespace AzureEz.ServiceBus;
+
+
+public static class ServiceCollectionExtensions
+{
+    /// <summary>
+    /// <para>Registers the AzureEz.ServiceBus helper class <see cref="IServiceBusFactory"/>.</para>
+    /// <para>Additionally this method also registers the base <see cref="Azure.Messaging.ServiceBus.ServiceBusClient"/> required for connecting to Azure ServiceBus.</para>
+    /// </summary>
+    /// <param name="services">The <see cref="IServiceCollection"/> instance to which the services should be added.</param>
+    /// <param name="configuration">Instance of <see cref="IConfiguration"/> containing ServiceBus connection string.</param>
+    public static IServiceCollection AddAzureEzServiceBus(this IServiceCollection services, IConfiguration configuration)
+    {
+        services.AddAzureClients(builder => builder.AddServiceBusClient(configuration));
+        services.AddSingleton<IServiceBusFactory, ServiceBusFactory>();
+        
+        return services;
+    }
+    
+    /// <summary>
+    /// <para>Registers the AzureEz.ServiceBus helper class <see cref="IServiceBusFactory"/>.</para> 
+    /// <para>Additionally this method also registers the base <see cref="Azure.Messaging.ServiceBus.ServiceBusClient"/> required for connecting to Azure ServiceBus.</para>
+    /// </summary>
+    /// <param name="services">The <see cref="IServiceCollection"/> instance to which the services should be added.</param>
+    /// <param name="connectionString">The connection string required to connect to Azure ServiceBus</param>
+    public static IServiceCollection AddAzureEzServiceBus(this IServiceCollection services, string connectionString)
+    {
+        services.AddAzureClients(builder => builder.AddServiceBusClient(connectionString));
+        services.AddSingleton<IServiceBusFactory, ServiceBusFactory>();
+        
+        return services;
+    }
+}
